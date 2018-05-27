e594c84996a8363d8d7ff5d792ea461effec2108
a.shibaev <2fasti.miem@gmail.com>
2018-05-27 18:55:04 +0300

    Release task

--- 14/5/main.cpp
+++ 14/5/main.cpp
@@ -15,6 +15,16 @@ using namespace std;
 void startGame(int &, int &);
 void initGrid(string **, int, int);
 void printGrid(string **, int, int);
+void playerMove(string **, int, int, int);
+bool checkWinner(string **, int, int);
+void flushGrid(string **, int);
+
+const string playerChars[] = {"+", "x"};
+
+enum PLAYERS {
+    PLUS = 0,
+    CROSS = 1,
+};
 
 void startGame(int &height, int &width) {
     while (height <= 3 || width <= 3) {
@@ -39,22 +49,134 @@ void initGrid(string ** p_p_grid, int height, int width) {
 void printGrid(string ** p_p_grid, int height, int width) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            cout << p_p_grid[i][j] << " ";
+            cout << p_p_grid[j][i] << " ";
         }
         cout << endl;
     }
+}
+
+void flushGrid(string ** p_p_grid, int h) {
+    for(int i = 0; i < h; i++) {
+        delete [] p_p_grid[i];
+    }
+    delete [] p_p_grid;
+}
+
+void playerMove(string ** p_p_grid, int height, int player) {
+    while (true) {
+        int c = 0;
+        cout << "Player " << player + 1 <<  " move!" << endl;
+        cout << "Column: ";
+        cin >> c;
+        cout << endl;
+        string * p_cell = 0;
+        for (int i = 0; i < height && (p_p_grid[c][i] == "_"); i++) {
+            p_cell =  &p_p_grid[c][i];
+        }
+        if (p_cell != NULL) {
+            *p_cell = playerChars[player];
+            break;
+        } else cout << "Do step again!" << endl;
+    }
+
+}
+
+bool checkWinner(string ** p_p_grid, int height, int width) {
+    for (int player = 0; player < 2; player++) {
+        string chart = playerChars[player];
+
+        //обход по строкам
+        int horisontal_res = 0;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (p_p_grid[i][j] == chart) {
+                    horisontal_res++;
+                } else {
+                    horisontal_res = 0;
+                }
+                if (horisontal_res >= 4) {
+                    cout << "Player " << player + 1 << " won!" << endl;
+                    return true;
+                }
+            }
+        }
+        //обход по столбцам
+        int vertical_res = 0;
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (p_p_grid[j][i] == chart) {
+                    vertical_res++;
+                } else {
+                    vertical_res = 0;
+                }
+                if (vertical_res >= 4) {
+                    cout << "Player " << player + 1 << " won!" << endl;
+                    return true;
+                }
+            }
+        }
+        //обход по обратным правым диагоналям
+        int rdiagonal_res = 0;
+        for (int r = 0; r < height + width - 1; r++)
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                    if (i + j == r) {
+                        if (p_p_grid[j][i] == chart) {
+                            rdiagonal_res++;
+                        } else {
+                            rdiagonal_res = 0;
+                        }
+                        if (rdiagonal_res >= 4) {
+                            cout << "Player " << player + 1 << " won!" << endl;
+                            return true;
+                        }
+
+        }
+        //обход по обратным левым диагоналям
+        int ldiagonal_res = 0;
+        for (int r = 0; r < height + width - 1; r++)
+            for (int i = 0; i < height; i++)
+                for (int j = width - 1; j >=1 ; j--)
+                    if (i + j == r) {
+                        if (p_p_grid[j][i] == chart) {
+                            ldiagonal_res++;
+                        } else {
+                            ldiagonal_res = 0;
+                        }
+                        if (rdiagonal_res >= 4) {
+                            cout << "Player " << player + 1 << " won!" << endl;
+                            return true;
+                        }
+
+                    }
+    }
+    return false;
 }
 
 int main() {
     int h = 0, w = 0;
     startGame(h, w);
 
+    bool isGameContinue = true;
     //init pointer to array
-    string ** p_p_Grid = new string * [h];
+    auto ** p_p_Grid = new string * [h];
     for (int i = 0; i < h; i++) {
         p_p_Grid[i] = new string [w];
     }
     initGrid(p_p_Grid, h, w);
     printGrid(p_p_Grid, h, w);
+
+    while (isGameContinue) {
+        for (int i = PLUS; i <= CROSS; i++) {
+            playerMove(p_p_Grid, h, i);
+            printGrid(p_p_Grid, h, w);
+            isGameContinue = !checkWinner(p_p_Grid, h, w);
+            if (!isGameContinue) {
+                break;
+            }
+        }
+    }
+    flushGrid(p_p_Grid, h);
+    cout << "END GAME!";
     return 0;
 }
